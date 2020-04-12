7d53af0f80680c07aaf926f57bc96d06902c2865
d1k11 <kdv0988@gmail.com>
2020-04-12 23:57:48 +0300

    refactoring gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -8,47 +8,47 @@ export const randomExpressionGen = () => {
   return `${num1} ${num2}`;
 };
 
+const factorizator = (num, div = 2, arr = [1]) => {
+  if (num === 1) {
+    return arr;
+  }
+  if (num % div === 0) {
+    arr.push(div);
+    factorizator(num / div, div, arr);
+  } else {
+    factorizator(num, div + 1, arr);
+  }
+  return arr;
+};
+
+const getIntersectionOfArrays = (arr1, arr2) => {
+  const size1 = arr1.length;
+  const size2 = arr2.length;
+  let index1 = 0;
+  let index2 = 0;
+  const result = [];
+  while (index1 < size1 && index2 < size2) {
+    if (arr1[index1] === arr2[index2]) {
+      result.push(arr1[index1]);
+      index1 += 1;
+      index2 += 1;
+    } else if (arr1[index1] > arr2[index2]) {
+      index2 += 1;
+    } else {
+      index1 += 1;
+    }
+  }
+  return result;
+};
+
 export const prepareExp = (string) => {
   const strToArr = string.split([' ']);
   const strToNum = (i) => Number(strToArr[i]);
   const num1 = strToNum(0);
   const num2 = strToNum(1);
 
-  const factorizator = (num, div = 2, arr = [1]) => {
-    if (num === 1) {
-      return arr;
-    }
-    if (num % div === 0) {
-      arr.push(div);
-      factorizator(num / div, div, arr);
-    } else {
-      factorizator(num, div + 1, arr);
-    }
-    return arr;
-  };
-
   const mltplrsOfNum1 = factorizator(num1);
   const mltplrsOfNum2 = factorizator(num2);
-
-  const getIntersectionOfArrays = (arr1, arr2) => {
-    const size1 = arr1.length;
-    const size2 = arr2.length;
-    let index1 = 0;
-    let index2 = 0;
-    const result = [];
-    while (index1 < size1 && index2 < size2) {
-      if (arr1[index1] === arr2[index2]) {
-        result.push(arr1[index1]);
-        index1 += 1;
-        index2 += 1;
-      } else if (arr1[index1] > arr2[index2]) {
-        index2 += 1;
-      } else {
-        index1 += 1;
-      }
-    }
-    return result;
-  };
 
   const intersection = getIntersectionOfArrays(mltplrsOfNum1, mltplrsOfNum2);
   const reducer = (acc, currentValue) => acc * currentValue;
